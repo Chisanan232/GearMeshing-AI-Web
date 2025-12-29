f3f62b388ee467329b110542b7e4952855230185
Chisanan232 <chi10211201@cycu.org.tw>
2025-12-29 18:29:31 +0800

    ➕ Add the Prettier configuration.

---
+++ .prettierrc.config.js
@@ -0,0 +1,22 @@
+// prettier.config.js
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    plugins: [
+        '@trivago/prettier-plugin-sort-imports',
+        'prettier-plugin-tailwindcss', // 必須放在最後
+    ],
+    importOrder: [
+        '^react',
+        '^next',
+        '<THIRD_PARTY_MODULES>',
+        '^@/components/(.*)$',
+        '^@/lib/(.*)$',
+        '^@/store/(.*)$',
+        '^[./]',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
