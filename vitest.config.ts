1771759ceffb9fae7e2064578efc091a86b41b1a
Chisanan232 <chi10211201@cycu.org.tw>
2026-01-02 10:00:50 +0800

    ➕ Add more detail settings for the test coverage reports.

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov", "json", "html"],
-      reportsDirectory: './coverage',
+      reportsDirectory: "./coverage",
       exclude: [
         "node_modules/",
         "tests/",
